0a2c3fd66fd94722dab4e77251f6b33d29185cd8
FaydSpear <fspea123@gmail.com>
2020-08-30 23:37:19 +1000

    fixed user

--- backend/api.js
+++ backend/api.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 
-const User = require('./models/User')
+const User = require('models/User')
 
 router.get('/test', (req, res) => {
     User.query().then(users => {
