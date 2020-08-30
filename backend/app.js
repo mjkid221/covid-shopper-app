d0db54b58b98433441a6211fc82e21ce74080ae3
FaydSpear <fspea123@gmail.com>
2020-08-30 23:26:27 +1000

    testing objection

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,20 @@
 const express = require('express')
 
+var knex = require('knex')({
+  client: 'mysql',
+  connection: {
+    host : '127.0.0.1',
+    user : 'root',
+    password : '757583a3b3a4931b',
+    database : 'test'
+  }
+});
+
 const app = express()
 const port = 8081
 
 app.set('trust proxy', 'loopback');
-
-app.get('/test', (req, res) => {
-  res.send('This is a test endpoint')
-})
+app.use('/api', require('./api').router)
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`)
