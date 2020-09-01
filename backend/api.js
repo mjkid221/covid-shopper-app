58a6177cdd7cdb9ce79021ce19c1f2d4d7295593
FaydSpear <fspea123@gmail.com>
2020-09-02 00:29:30 +1000

    fixed List relational mappings

--- backend/api.js
+++ backend/api.js
@@ -11,9 +11,36 @@ router.get('/test', (req, res) => {
     })
 })
 
-router.get('/dog', (req, res) => {
-    res.json({test: 1})
+/* Example of how to retreive customer with a given id */
+router.get('/customer/:id', (req, res) => {
+    Customer.query().findById(req.params.id).then(customer => {
+        res.json(customer)
+    }).catch(e => {
+        res.send(e)
+    })
 })
+
+/* Example of how to retrieve all of a customers lists */
+router.get('/customer/:id/lists', (req, res) => {
+    const customer = await Customer.query().findById(req.params.id)
+    customer.$relatedQuery('lists').then(lists => {
+        res.json(lists)
+    }).catch(e => {
+        res.send(e)
+    })
+})
+
+/*
+Alteratively (for the above endpoint) you could do:
+
+router.get('/customer/:id/lists', (req, res) => {
+    Customer.relatedQuery('lists').for(req.params.id).then(lists => {
+        res.json(lists)
+    }).catch(e => {
+        res.send(e)
+    })
+})
+*/
 
 module.exports = {
     router: router
