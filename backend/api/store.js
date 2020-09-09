c51040d9de5c7de075350e74fb4a774981303b4d
FaydSpear <fspea123@gmail.com>
2020-09-10 00:26:44 +1000

    fix can supply list endpoint

--- backend/api/store.js
+++ backend/api/store.js
@@ -36,13 +36,15 @@ router.get('/can-supply-list/:id', async (req, res) => {
                  .where('s.product_quantity', '<=', 'Stocks.quantity')
                  .where('s.product_quantity', '<=', 'Stocks.purchase_limit')
     */
+    let count = await ListProduct.query().count().where('list_id', req.params.id)
+    console.log(count)
     Stock.query().alias('t').select('t.store_id')
                  .join('Shopping_List_Products as s', 's.product_id', 't.product_id')
                  .where('s.list_id', req.params.id)
                  .whereRaw('s.product_quantity <= t.quantity')
                  .whereRaw('s.product_quantity <= t.purchase_limit')
                  .groupBy('t.store_id')
-                 .havingRaw('count(*) > ?', ListProduct.query().count().where('list_id', req.params.id))
+                 .havingRaw('count(*) = ?', count)
                  .then(stores => res.json(stores))
                  .catch(e => res.send(e))
 
