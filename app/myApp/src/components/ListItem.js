70922707885b9bfb8fed42cfd8c13a769e36b00f
FaydSpear <fspea123@gmail.com>
2020-10-01 02:28:05 +1000

    action-sheet for deleting item

--- app/myApp/src/components/ListItem.js
+++ app/myApp/src/components/ListItem.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
     IonContent,
     IonHeader,
@@ -22,19 +22,44 @@ import {
     IonCheckbox,
     IonFabButton,
     IonFab,
-    IonIcon
+    IonIcon,
+    IonSelectOption,
+    IonSelect,
+    IonActionSheet
 } from '@ionic/react';
+import { trash } from 'ionicons/icons';
 
 const ListItem = ({callback, index, product}) => {
     let pcount = product.product_quantity
     let pname = product.product_name
     let pid = product.product_id
+
+    const [showActions, setShowActions] = useState(false)
+
     return (
         <IonItemSliding>
-            <IonItem key={index}>
+            <IonItem key={index} onHold={() => setShowActions(true)}>
                 <IonCheckbox slot='start'></IonCheckbox>
-                <IonLabel style={{textAlign: "center"}}>{pcount + " x " + pname}</IonLabel>
+                <IonLabel
+                    style={{textAlign: "center"}}
+                >
+                {pcount + " x " + pname}
+                </IonLabel>
             </IonItem>
+            <IonActionSheet
+                isOpen={showActions}
+                onDidDismiss={() => setShowActions(false)}
+                cssClass='my-custom-class'
+                buttons={[{
+                  text: 'Delete',
+                  role: 'destructive',
+                  icon: trash,
+                  handler: () => {
+                    console.log('Delete clicked');
+                  }
+                }]}
+            >
+            </IonActionSheet>
             <IonItemOptions side='start'>
                 <IonItemOption
                     color="primary"
