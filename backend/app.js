eee8b866df2b9b26c02da28a9a4370933b8817df
FaydSpear <fspea123@gmail.com>
2020-09-30 14:18:18 +1000

    bug fix with customer endpoint

--- backend/app.js
+++ backend/app.js
@@ -23,10 +23,11 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.set('trust proxy', 'loopback')
 
-app.use('/', require('./api').router)
+//app.use('/', require('./api').router)
 app.use('/list', require('./api/list'))
 app.use('/product', require('./api/product'))
 app.use('/store', require('./api/store'))
+app.use('/customer', require('./api/customer'))
 
 const port = 8081
 app.listen(port, () => {
