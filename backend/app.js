3d6501cbb2da1311feddb5619f82051447fcd473
root <root@dreamteam.zones.eait.uq.edu.au>
2020-10-16 07:52:14 +0000

    add api to urls

--- backend/app.js
+++ backend/app.js
@@ -24,10 +24,10 @@ app.use(bodyParser.json())
 app.set('trust proxy', 'loopback')
 
 //app.use('/', require('./api').router)
-app.use('/list', require('./api/list'))
-app.use('/product', require('./api/product'))
-app.use('/store', require('./api/store'))
-app.use('/customer', require('./api/customer'))
+app.use('/api/list', require('./api/list'))
+app.use('/api/product', require('./api/product'))
+app.use('/api/store', require('./api/store'))
+app.use('/api/customer', require('./api/customer'))
 
 const port = 8081
 app.listen(port, () => {
